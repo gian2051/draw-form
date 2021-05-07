fb703171ad90f4cbe71e4ca768cc906db3dba5be
gian2051 <giangbd5@hotmail.com>
2021-05-07 15:26:09 -0500

    arrastre de controles
    Arrastrar los controles del lado izquierdo (haciendo clic) al lado derecho, de tal manera que se pueda ir diseñando el orden de los controles en un web form

---
+++ diseño/Form1.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ToolStripContainer1 = New System.Windows.Forms.ToolStripContainer()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.lblControl = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblMouse = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripSplitButton1 = New System.Windows.Forms.ToolStripSplitButton()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripContainer1.BottomToolStripPanel.SuspendLayout()
+        Me.ToolStripContainer1.ContentPanel.SuspendLayout()
+        Me.ToolStripContainer1.TopToolStripPanel.SuspendLayout()
+        Me.ToolStripContainer1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStripContainer1
+        '
+        '
+        'ToolStripContainer1.BottomToolStripPanel
+        '
+        Me.ToolStripContainer1.BottomToolStripPanel.Controls.Add(Me.StatusStrip1)
+        '
+        'ToolStripContainer1.ContentPanel
+        '
+        Me.ToolStripContainer1.ContentPanel.Controls.Add(Me.SplitContainer1)
+        Me.ToolStripContainer1.ContentPanel.Size = New System.Drawing.Size(813, 462)
+        Me.ToolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ToolStripContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStripContainer1.Name = "ToolStripContainer1"
+        Me.ToolStripContainer1.Size = New System.Drawing.Size(813, 508)
+        Me.ToolStripContainer1.TabIndex = 0
+        Me.ToolStripContainer1.Text = "ToolStripContainer1"
+        '
+        'ToolStripContainer1.TopToolStripPanel
+        '
+        Me.ToolStripContainer1.TopToolStripPanel.Controls.Add(Me.MenuStrip1)
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Dock = System.Windows.Forms.DockStyle.None
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lblControl, Me.lblMouse, Me.ToolStripSplitButton1, Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(813, 22)
+        Me.StatusStrip1.TabIndex = 0
+        '
+        'lblControl
+        '
+        Me.lblControl.Name = "lblControl"
+        Me.lblControl.Size = New System.Drawing.Size(60, 17)
+        Me.lblControl.Text = "Desarrollo"
+        '
+        'lblMouse
+        '
+        Me.lblMouse.Name = "lblMouse"
+        Me.lblMouse.Size = New System.Drawing.Size(0, 17)
+        '
+        'ToolStripSplitButton1
+        '
+        Me.ToolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripSplitButton1.Image = CType(resources.GetObject("ToolStripSplitButton1.Image"), System.Drawing.Image)
+        Me.ToolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripSplitButton1.Name = "ToolStripSplitButton1"
+        Me.ToolStripSplitButton1.Size = New System.Drawing.Size(32, 20)
+        Me.ToolStripSplitButton1.Text = "ToolStripSplitButton1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(119, 17)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.RadioButton1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.CheckBox1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.LinkLabel1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TextBox1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.BackColor = System.Drawing.Color.White
+        Me.SplitContainer1.Size = New System.Drawing.Size(813, 462)
+        Me.SplitContainer1.SplitterDistance = 152
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(19, 109)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(90, 17)
+        Me.RadioButton1.TabIndex = 5
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(19, 84)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(16, 59)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(59, 13)
+        Me.LinkLabel1.TabIndex = 3
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "LinkLabel1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(19, 164)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.TextBox1.Location = New System.Drawing.Point(19, 132)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Dock = System.Windows.Forms.DockStyle.None
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(813, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.ArchivoToolStripMenuItem.Text = "Archivo"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(813, 508)
+        Me.Controls.Add(Me.ToolStripContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ToolStripContainer1.BottomToolStripPanel.ResumeLayout(False)
+        Me.ToolStripContainer1.BottomToolStripPanel.PerformLayout()
+        Me.ToolStripContainer1.ContentPanel.ResumeLayout(False)
+        Me.ToolStripContainer1.TopToolStripPanel.ResumeLayout(False)
+        Me.ToolStripContainer1.TopToolStripPanel.PerformLayout()
+        Me.ToolStripContainer1.ResumeLayout(False)
+        Me.ToolStripContainer1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ToolStripContainer1 As ToolStripContainer
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents lblControl As ToolStripStatusLabel
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblMouse As ToolStripStatusLabel
+    Friend WithEvents ToolStripSplitButton1 As ToolStripSplitButton
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+End Class
---
+++ diseño/Form1.vb
@@ -0,0 +1,153 @@
+﻿Public Class Form1
+
+    Private controlActual As Control = Nothing
+    Private mouseInicial As Point
+    Private ubicaInicial As Point
+
+#Region "Eventos de la plantilla de contorles"
+
+    Private Sub presionarClic(sender As Object, e As MouseEventArgs)
+        'al hacer clic sin soltar el clic izquierdo
+        If e.Button = MouseButtons.Left Then
+            controlActual = sender 'contenedor.Controls.Find(sender.ToString, False)(0)
+            ubicaInicial = controlActual.Location
+            mouseInicial = e.Location
+        End If
+    End Sub
+
+    Private Sub moverMouse(sender As Object, e As MouseEventArgs)
+        'al mover el mouse, solo se activa si se mantiene presionado el clic izquierdo
+        If controlActual IsNot Nothing Then
+            Dim puntoControl As Point = controlActual.Location 'ubicacion del control
+            Dim puntoCursor As Point = e.Location 'donde esta el 
+
+            Dim nuevaUbicacion As Point
+            nuevaUbicacion.X = puntoControl.X + puntoCursor.X - mouseInicial.X
+            nuevaUbicacion.Y = puntoControl.Y + puntoCursor.Y - mouseInicial.Y
+
+            controlActual.Location = nuevaUbicacion
+            lblControl.Text = controlActual.Location.ToString
+
+            If contenidoEnDiseñador() Then
+                controlActual.BringToFront()
+            End If
+        End If
+
+    End Sub
+
+    Private Sub soltarMouse(sender As Object, e As MouseEventArgs)
+        'se ejecuta cuando se suelta el boton del mouse
+        If controlActual Is Nothing Then
+            Exit Sub
+        End If
+
+        Dim nuevaUbica As Point = controlActual.Location
+        nuevaUbica.X -= SplitContainer1.Panel1.Size.Width
+        Dim mm As Object = controlActual.GetType()
+        Dim coordenada As Point = controlActual.Location
+        If contenidoEnDiseñador() Then
+            crear(controlActual)
+            Dim nuevo As Control = crear(controlActual)
+            nuevo.Location = nuevaUbica
+            SplitContainer1.Panel2.Controls.Add(nuevo)
+            controles.Add(nuevo)
+        End If
+
+        controlActual.Location = ubicaInicial
+        controlActual = Nothing
+
+        lblControl.Text = coordenada.ToString
+    End Sub
+
+    Private Function crear(ByVal original As Control) As Control
+        Dim nuevo As Control
+        Select Case TypeName(controlActual)
+            Case "TextBox"
+                nuevo = New TextBox
+            Case "LinkLabel"
+                nuevo = New LinkLabel
+            Case "CheckBox"
+                nuevo = New CheckBox
+            Case "RadioButton"
+                nuevo = New RadioButton
+            Case "Button"
+                nuevo = New Button
+            Case Else
+                nuevo = New Label
+        End Select
+
+        nuevo.Name = "dibuja" & controles.Count + 1
+        nuevo.Size = original.Size
+        nuevo.ForeColor = original.ForeColor
+        nuevo.BackColor = original.BackColor
+        nuevo.Text = original.Text
+
+        Return nuevo
+    End Function
+
+#End Region
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        contenedor = SplitContainer1.Panel2
+        corregirX = SplitContainer1.Panel1.Size.Width
+
+        AddHandler Label1.MouseDown, AddressOf presionarClic
+        AddHandler Label1.MouseMove, AddressOf moverMouse
+        AddHandler Label1.MouseUp, AddressOf soltarMouse
+
+        AddHandler LinkLabel1.MouseDown, AddressOf presionarClic
+        AddHandler LinkLabel1.MouseMove, AddressOf moverMouse
+        AddHandler LinkLabel1.MouseUp, AddressOf soltarMouse
+
+        AddHandler CheckBox1.MouseDown, AddressOf presionarClic
+        AddHandler CheckBox1.MouseMove, AddressOf moverMouse
+        AddHandler CheckBox1.MouseUp, AddressOf soltarMouse
+
+        AddHandler RadioButton1.MouseDown, AddressOf presionarClic
+        AddHandler RadioButton1.MouseMove, AddressOf moverMouse
+        AddHandler RadioButton1.MouseUp, AddressOf soltarMouse
+
+        AddHandler TextBox1.MouseDown, AddressOf presionarClic
+        AddHandler TextBox1.MouseMove, AddressOf moverMouse
+        AddHandler TextBox1.MouseUp, AddressOf soltarMouse
+
+        AddHandler Button1.MouseDown, AddressOf presionarClic
+        AddHandler Button1.MouseMove, AddressOf moverMouse
+        AddHandler Button1.MouseUp, AddressOf soltarMouse
+    End Sub
+
+    'siempre que los controles esten a la izquierda y el diseño del form a la derecha
+    Private controles As New List(Of Control)
+    Private contenedor As Control
+    Private corregirX As Integer
+
+    Private Function contenidoEnDiseñador()
+        If controlActual Is Nothing Then
+            Return False
+        End If
+
+        Dim ini As Point = contenedor.Location
+        Dim fin As Point = contenedor.Size
+        fin.X += ini.X
+        fin.Y += ini.Y
+
+        Dim coordenada As Point = controlActual.Location
+        If coordenada.X >= ini.X AndAlso coordenada.X <= fin.X Then
+            If coordenada.Y <= fin.Y AndAlso coordenada.Y >= ini.Y Then
+                Return True
+            End If
+        End If
+
+        Return False
+    End Function
+
+End Class
+
+Class prueba
+    'MouseCaptureChanged= "mouse capture changed"
+    'MouseClick= "mouse clic"
+    'MouseDoubleClick= "mouse doble clic"
+    'MouseEnter= "cursor entra al control"
+    'MouseHover= "cursor entra al control"
+    'MouseLeave= "cursosr sale del control"
+End Class
